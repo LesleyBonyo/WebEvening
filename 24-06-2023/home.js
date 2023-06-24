daace5634c65cda4c57b0e023cef54c59b98194e
LesleyBonyo <lesleybonyo@gmail.com>
2023-06-24 21:44:39 +0300

    JavaScript HTML DOM

---
+++ 24-06-2023/home.js
@@ -0,0 +1,10 @@
+	// Document Object Model
+//getting html elements
+//get by element id
+let myPar = 
+document.getElementById('mypar');
+console.log(myPar);
+//get by tag name
+let allParagraphs = 
+document.getElementsByTagName('p');
+console.log(allParagraphs[1]);
