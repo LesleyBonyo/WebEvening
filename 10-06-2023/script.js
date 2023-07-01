e8da0a3bd269bbc704b2d06f19ebf6e33888ecce
LesleyBonyo <lesleybonyo@gmail.com>
2023-07-01 13:42:17 +0300

    JavaScript Form Validation

--- 10-06-2023/script.js
+++ 10-06-2023/script.js
@@ -8,12 +8,22 @@ function validateForm(){
 	let result2 = document.getElementById('result2');
 	if (userName == "" || password == "") {
 		alert("Fields should not be empty");
-	}else if (userName.match(/^[a-z]+$/)) {
-		result1.innerHTML = "username should have lowecase alphabets";
-	} else if (password.length < 8){
-		result2.innerHTML = "password should have at least 8 characters";
-	}else{
-		alert("success");
+		return false;
 	}
-	return false;
+	if (!userName.match(/^[a-z]+$/)) {
+		result1.innerHTML = "username should have lowecase alphabets";
+		return false;
+	} else{
+		result1.innerHTML = "";
+	}
+	if (password.length < 8){
+		result2.innerHTML = "password should have at least 8 characters";
+		return false;
+	}else{
+		result1.innerHTML = "";
+	}
+		
+	alert("success");
+	return true;
+	
 }
